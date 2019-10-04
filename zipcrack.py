b50a9ae6e27101daeb1ef4f8206ba7f048446439
Vaibhav Choudhari <vaibhavchoudhari05@gmail.com>
2019-10-05 04:08:57 +0530

    Add files via upload
    Wordlist bases zip cracker coded in Python 3.7.
    Includes threading for faster execution.

---
+++ zipcrack.py
@@ -0,0 +1,34 @@
+import zipfile
+from threading import Thread
+
+def extract(file, password):
+	
+	try:
+		file.extractall(path=None, members=None, pwd=password)
+		print("[+] Zip file extracted. \nPassword = "+ str(password).strip("b'"))
+		exit()
+
+	except RuntimeError:
+		pass
+		#print("Trying password: "+ str(password).strip("b'"))
+		
+
+def main():
+	
+	file = zipfile.ZipFile("test.zip")
+
+	f = open("wordlist.txt", "r")
+	for i in f.readlines():
+		password = (i.strip()).encode()
+		#print(password)
+
+		t = Thread(target=extract, args=(file, password))
+		t.start()
+		#extract(file, password)
+	
+
+		
+		
+
+if __name__ == '__main__':
+	main()
