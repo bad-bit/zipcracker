3b7f65f0c5ee609c28bab99f001421587fbcf60d
Vaibhav Choudhari <vaibhavchoudhari05@gmail.com>
2019-10-05 04:14:11 +0530

    Update zipcrack.py

--- zipcrack.py
+++ zipcrack.py
@@ -3,6 +3,7 @@
 import zipfile
 from threading import Thread
 
+#function to brute force zip file with the provided dictionary
 def extract(file, password):
 	
 	try:
@@ -17,19 +18,19 @@ def extract(file, password):
 
 def main():
 	
+	#open zipfile
 	file = zipfile.ZipFile("test.zip")
 
 	f = open("wordlist.txt", "r")
 	for i in f.readlines():
 		password = (i.strip()).encode()
+		#encoding because "extractall" method only takes byte like object and not string
 		#print(password)
-
+		
+		#thread execution
 		t = Thread(target=extract, args=(file, password))
 		t.start()
 		#extract(file, password)
-	
-
-		
 		
 
 if __name__ == '__main__':
